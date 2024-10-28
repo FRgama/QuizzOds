65d6998fbec3e20b94582bd622651a6eb6cd8dfe
FRgama <rodrigocgama04@gmail.com>
2024-10-28 17:05:12 -0300

    Inserido banco de questoes

--- app/src/main/java/com/example/quizzods/gameplay.java
+++ app/src/main/java/com/example/quizzods/gameplay.java
@@ -22,7 +22,7 @@ public class gameplay extends AppCompatActivity {
     private RadioButton questao1, questao2, questao3, questao4;
     private Button enviarResposta;
     private int idRespostaCorreta = R.id.questao1;
-    private TextView questaoNumero;
+    private TextView questaoNumero, questaoTexto;
     private TextView vidasRestantes;
 
 
@@ -33,10 +33,17 @@ public class gameplay extends AppCompatActivity {
                             "Igualdade de gênero",
                             "Energia limpa e acessível",
                             "Combate à fome e desnutrição"),
-                    1)
+                    1),
+            new Questao("O ODS 5 está relacionado a qual temática?",
+                        Arrays.asList("Água limpa",
+                                "Educação de qualidade",
+                                "Igualdade de gênero",
+                                "Ação contra a mudança global do clima"),
+                    2)
+            // Adicione outras questões conforme necessário
     );
 
-    private int numeroQuestao = 1;
+    private int numeroQuestao = 0;
     private int vidas = 3;
 
 
@@ -54,9 +61,12 @@ public class gameplay extends AppCompatActivity {
         questao3 = findViewById(R.id.questao3);
         questao4 = findViewById(R.id.questao4);
         vidasRestantes = findViewById(R.id.vidasRestantes);
+        questaoTexto = findViewById(R.id.questaoTexto);
 
         questaoNumero = findViewById(R.id.questaoNumero);
         enviarResposta = findViewById(R.id.btnEnviarResposta);
+
+        atualizarQuestao();
 
 
 
@@ -64,12 +74,22 @@ public class gameplay extends AppCompatActivity {
         enviarResposta.setOnClickListener(v -> {
             retornarCores();
             int idRespostaSelecionada = divQuestoes.getCheckedRadioButtonId(); //caso a resposta esteja correta
-            if (idRespostaSelecionada == idRespostaCorreta) {
+            int indiceRespostaSelecionada = divQuestoes.indexOfChild(findViewById(idRespostaSelecionada));
+            if (indiceRespostaSelecionada == questoes.get(numeroQuestao).getOpcaoCorreta()) {
                 Toast.makeText(this, "Resposta Correta!",Toast.LENGTH_SHORT).show();
                 findViewById(idRespostaSelecionada).setBackgroundColor(Color.GREEN);
                 numeroQuestao  = numeroQuestao+1;
                 questaoNumero.setText("Questão " + numeroQuestao);
-                enviarResposta.setEnabled(false);
+                if(numeroQuestao < questoes.size()){
+                    new android.os.Handler().postDelayed(() -> {
+                        retornarCores();
+                        atualizarQuestao();
+                    }, 1000);
+                }
+                else{
+                    Toast.makeText(this, "Fim de jogo!",Toast.LENGTH_SHORT).show();
+                    enviarResposta.setEnabled(false);
+                }
 
 
 
@@ -79,15 +99,33 @@ public class gameplay extends AppCompatActivity {
                 findViewById(idRespostaSelecionada).setBackgroundColor(Color.RED);
                 vidas -=1;
                 vidasRestantes.setText("Vidas Restantes: " + vidas);
+                if(vidas == 0){
+                    Toast.makeText(this, "Você Perdeu!",Toast.LENGTH_SHORT).show();
+                    enviarResposta.setEnabled(false);
+                }
+
             }
+            divQuestoes.clearCheck();
 
         });
+
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void atualizarQuestao(){
+        Questao questaoAtual = questoes.get(numeroQuestao);
+        questaoTexto.setText(questaoAtual.getEnunciado());
+
+        questao1.setText(questaoAtual.getAlternativas().get(0));
+        questao2.setText(questaoAtual.getAlternativas().get(1));
+        questao3.setText(questaoAtual.getAlternativas().get(2));
+        questao4.setText(questaoAtual.getAlternativas().get(3));
     }
 
     private void retornarCores(){
